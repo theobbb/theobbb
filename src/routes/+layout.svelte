4c06907a1415f5ea197b71380c76acef8b595368
Theo <theobbb64@gmail.com>
2025-09-17 08:59:03 -0400

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import '../app.css';
+
+	import favicon from '$lib/assets/favicon.svg';
+	import Header from './header.svelte';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+<Header />
+<div class="px-8">
+	{@render children?.()}
+</div>
---
+++ src/routes/header.svelte
@@ -0,0 +1,30 @@
+<script>
+	const links = [
+		{
+			name: 'accueil * home',
+			href: '/'
+		},
+		{
+			name: 'blog',
+			href: '/blog'
+		},
+		{
+			name: 'contact',
+			href: '/contact'
+		}
+	];
+</script>
+
+<header class=" px-4 py-2">
+	<nav class="">
+		<ul class="flex gap-8 text-lg uppercase">
+			{#each links as { name, href }}
+				<li>
+					<a class="border border-yellow-800 px-3 py-2 transition hover:bg-orange-500/20" {href}
+						>[{name}]</a
+					>
+				</li>
+			{/each}
+		</ul>
+	</nav>
+</header>
